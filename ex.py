e36d1cedd15218f72d30b9c65e7466d380993a08
Soudamini <96462870+soudaminimalik2001@users.noreply.github.com>
2022-05-10 22:54:50 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,29 @@
+# swapping method
+#1. temprary method
+# a=10
+# b=5
+# tem=a
+# # print(tem)
+# # print(a)
+# a=b
+# # print(a)
+# # print(b)
+# b=tem
+# print(a)      o/p-5
+# print(b)      o/p-10
+
+# 2. updating method
+# a=5
+# b=10
+# c=a+b
+# a=c-a
+# b=c-a
+# print(a)      o/p-10
+# print(b)      o/p-5
+
+# 3.interchanging method
+# a=10
+# b=5
+# a,b=b,a
+# print(a)
+# print(b)
